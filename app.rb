a504b0e8132f3a829cf2cc6fca62d8d63e77b789
kateleext <83829378+kateleext@users.noreply.github.com>
2025-02-02 21:52:18 +0000

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
+
+key = ENV.fetch("EXCHANGE_RATE_KEY")
+list_url = "https://api.exchangerate.host/list?access_key=" + key
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @currencies = JSON.parse(HTTP.get(list_url)).fetch("currencies").keys
+  erb(:home)
+end
+
+get("/:currency_one") do
+  @currency_one = params.fetch("currency_one")
+  @currencies = JSON.parse(HTTP.get(list_url)).fetch("currencies").keys
+  erb(:currency_one)
+end
+
+get("/:currency_one/:currency_two") do
+  @currency_one = params.fetch("currency_one")
+  @currency_two = params.fetch("currency_two")
+  convert_url = "https://api.exchangerate.host/convert?from=#{@currency_one}&to=#{@currency_two}&amount=1&access_key=" + key
+  @result = JSON.parse(HTTP.get(convert_url)).fetch("result")
+  erb(:currency_two)
 end
